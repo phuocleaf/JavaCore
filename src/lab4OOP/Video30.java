8fec1be88ba849dbdd1ba0d98bcb5d49ded893fa
Phuoc <supermonster09@gmail.com>
2025-12-09 08:58:40 +0700

    lab oop

---
+++ src/lab4OOP/Video30.java
@@ -0,0 +1,12 @@
+package lab4OOP;
+
+public class Video30 {
+	public static void main(String[] args) {
+		System.out.println("run product");
+
+		Product test = new Product();
+		Product pr1 = test.nhapThongTin("Sky 2 gx", 1290000, 0.1);
+		test.xuatThongTin(pr1);
+		System.out.println("tax: " + test.getTaxPrice(pr1.getPrice(), pr1.getTax()));
+	}
+}
